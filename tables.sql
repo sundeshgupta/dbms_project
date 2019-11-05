c9b245e80ee7fb20088cac3d8d2775364a59865f
LunaticBoxer75 <aniketsangwan5@gmail.com>
2019-11-06 01:31:29 +0530

    F

--- tables.sql
+++ tables.sql
@@ -25,16 +25,17 @@ CREATE TABLE Login
 
 CREATE TABLE ArticlePage
 (
-	Article_id INT,
+	Article_id INT AUTO_INCREMENT,
 	Title VARCHAR(255),
 	Creation_date DATE,
-	Contributor_id INT,
-	PRIMARY KEY (Article_id)
+	Contributor_email VARCHAR(50),
+	PRIMARY KEY (Article_id),
+	FOREIGN KEY (Contributor_email) REFERENCES User(Email)
 );
 
 CREATE TABLE Tag
 (
-	Tag_id INT,
+	Tag_id INT AUTO_INCREMENT,
 	Name VARCHAR(25),
 	PRIMARY KEY (Tag_id)
 );
@@ -43,8 +44,8 @@ CREATE TABLE Rating
 (
 	Article_id INT,
 	Weight INT,
-	Contributor_id INT,
-	PRIMARY KEY (Article_id,  Weight, Contributor_id),
+	Contributor_email VARCHAR(50),
+	PRIMARY KEY (Article_id,  Weight, Contributor_email),
 	FOREIGN KEY (Article_id) REFERENCES ArticlePage(Article_id)
 );
 
@@ -87,7 +88,7 @@ CREATE TABLE Comment
 (
 	Comment_id INT,
 	Title VARCHAR(255),
-	Contributor_id INT,
+	Contributor_email VARCHAR(50),
 	Comment_date DATE,
 	Description VARCHAR(255),
 	PRIMARY KEY (Comment_id)
@@ -110,11 +111,33 @@ CREATE TABLE CommentFor
 	FOREIGN KEY (Comment_id) REFERENCES Comment(Comment_id),
 	FOREIGN KEY (CommentFor_id) REFERENCES Comment(Comment_id)
 );
+
+
 DELIMITER $$
 CREATE PROCEDURE get_user_data(usersname varchar(25))
 BEGIN
 SELECT PhoneNoDetails.Phone_no,T.Email_add,T.uname from PhoneNoDetails inner join (SELECT Login.Email as Email_add,User.Name as uname from Login inner join User on User.Email=Login.Email where Login.Username=usersname) T on Email_add=Email;
+END$$	
+
+CREATE TRIGGER RATING_UPDATE
+BEFORE INSERT ON Rating
+FOR EACH ROW
+BEGIN
+DELETE FROM RATING WHERE Contributor_email=NEW.contributor_email;
 END$$
-DELIMITER ;	
 
+CREATE TRIGGER After_Article_Insertion
+BEFORE INSERT ON ArticlePage
+FOR EACH ROW
+BEGIN
+INSERT INTO ViewsOrManages VALUES (NEW.contributor_email,NEW.article_id);
+SET NEW.Creation_date=CURDATE();
+END$$
 
+CREATE TRIGGER COMMENT_INSERT
+BEFORE INSERT ON Comment
+FOR EACH ROW
+BEGIN
+SET NEW.Comment_date=CURDATE();
+END$$
+DELIMITER ;
--- tables_data.sql
+++ tables_data.sql
@@ -2,11 +2,11 @@ INSERT INTO User VALUES ('agill@gmail.com','Ankit Gill',0),
 						('mmehta@yahoo.com','Mohit Mehta',0),
 						('hm5656@gmail.com','Himanshu Mishra',0);
 
-INSERT INTO PhoneNoDetails VALUES ('agill@gmail.com',9996161174),
-								  ('mmehta@yahoo.com',7404572035),
-								  ('agill@gmail.com',9865721721),
-								  ('hm5656@gmail.com',9996536939),
-								  ('hm5656@gmail.com',8950006509);
+INSERT INTO PhoneNoDetails VALUES ('agill@gmail.com','9996161174'),
+								  ('mmehta@yahoo.com','7404572035'),
+								  ('agill@gmail.com','9865721721'),
+								  ('hm5656@gmail.com','9996536939'),
+								  ('hm5656@gmail.com','8950006509');
 
 INSERT INTO Login VALUES ('agill@gmail.com','ilovesql','gilllogins'),
 						 ('mmehta@yahoo.com','mohit1234','mehta.mohit'),
